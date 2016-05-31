540dbfeda443ee7784cc9ece858785d529ac24c9
Josef Weidendorfer <Josef.Weidendorfer@gmx.de>
2016-05-31 10:38:13 +0200

    "leave" test. No need to implement "leavew"...

---
+++ tests/cases/decode/it-leave.s
@@ -0,0 +1,12 @@
+//!driver = test-driver-decode.c
+//!cc = cc
+.intel_syntax noprefix
+    .text
+    .globl  f1
+    .type   f1, @function
+f1:
+    // LEAVE instructions, Intel Vol. 2A 3-483
+    leave
+    // leavew // no need to implement 16-bit address-mode
+
+    ret
