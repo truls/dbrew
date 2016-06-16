734c3aa77ea42f1fb80463565b9b48444bd889aa
Alexis Engelke <alexis.engelke@googlemail.com>
2016-06-16 12:09:47 +0200

    ll: Decode test only in debug mode
    This allows us to use Clang for the compilation, too.
    
    On the other hand, I'm not quite sure whether testing multiple compilers
    makes sense at all, since things should "just work" via the C standard..
    ...oh, wait we catch some Clang assembler bugs this way.

--- llvm/tests/test-driver.c
+++ llvm/tests/test-driver.c
@@ -139,7 +139,9 @@ test_llvm_generation(bool debug)
     // We want to decode the output properly.
     dbrew_config_function_setname(dbrewDecoder, (uintptr_t) testCase.function, "?");
     dbrew_config_function_setsize(dbrewDecoder, (uintptr_t) testCase.function, 0x1000);
-    dbrew_verbose(dbrewDecoder, true, false, false);
+
+    // Print decoded block only in debug mode
+    dbrew_verbose(dbrewDecoder, debug, false, false);
 
     TestRoutine testRoutine = testRoutines[(int) testCase.routineIndex];
 
