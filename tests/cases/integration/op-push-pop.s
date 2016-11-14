d158d8b9e7647d0437619c4514fee6ce1e0b213d
Truls Asheim <truls@asheim.dk>
2016-11-14 16:27:06 +0100

    Add decoder support and test case for pushing imm16

--- tests/cases/integration/op-push-pop.s
+++ tests/cases/integration/op-push-pop.s
@@ -3,16 +3,16 @@
         .globl  f1
         .type   f1, @function
 f1:
-        push    0x5b
+        push     0x5b
         push    -0xAAAAAA
         push    -0x10
         mov     rbx, 0xBBB
         push    bx
-        push    0xCCC
-        mov     rbx, 0xDDDDDDDDDDDD
+        .byte   0x66, 0x68, 0xCC, 0x0C # Manual assembly of pushw 0xCCC (couldn't get GNU as to generate it)
+        movq    rbx, 0xDDDDDDDDDDDD # clang assembler needs q postfix
         push    rbx
         pop     r9
-        pop     rdx
+        pop     dx
         pop     cx
         pop     r10
         pop     rbx
@@ -21,5 +21,5 @@ f1:
         add     rax, rbx
         add     rax, r10
         add     rax, rcx
-        add     rax, rdx
+        add     rax, rdx # rax should be 0xdddddd334c05
         ret
