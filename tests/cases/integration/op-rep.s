56807e8b8d31386c7d1eb42d5aac1b7b35243cd2
Truls Asheim <truls@asheim.dk>
2017-01-10 18:47:43 +0100

    Clarified comments in op-rep test and don't render bytes in output

--- tests/cases/integration/op-rep.s
+++ tests/cases/integration/op-rep.s
@@ -1,13 +1,15 @@
+//!args=--nobytes
         .intel_syntax noprefix
         .text
         .globl  f1
         .type   f1, @function
 f1:
+        // Test dynamic parameters
         mov rsi, src
         mov rdi, dst
         mov rcx, 8
         rep movsb
-        // See if we correctly resurrect static vars
+        // Test if static parameters are correctly loaded.
         mov rsi, 0xAAAA
         mov rdi, 0xBBBB
         mov rcx, 8
